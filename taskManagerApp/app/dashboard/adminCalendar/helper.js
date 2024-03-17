2c37b1c3b8f46a949a6f8c253852e31eccc0840f
TylerTLe <tyler.le@edu.sait.ca>
2024-03-17 15:04:13 -0600

    Refactor generateResourceData function to ensure enough colors for users. as well as consistent colour each time admin logs into page

--- taskManagerApp/app/dashboard/adminCalendar/helper.js
+++ taskManagerApp/app/dashboard/adminCalendar/helper.js
@@ -1,16 +1,33 @@
-
 export default function generateResourceData(users) {
-    var data = [];
-    var colors = [
-        '#ff8787', '#9775fa', '#748ffc', '#3bc9db', '#69db7c', '#fdd835', '#748ffc',
-        '#9775fa', '#df5286', '#7fa900', '#fec200', '#5978ee', '#00bdae', '#ea80fc'
-    ];
-    return users.map((user, index) => {
-        const colorIndex = Math.floor(Math.random() * colors.length);
-        return {
-            Id: index + 1, // Assign a new ID based on array index
-            Text: user.name, // Assuming 'name' is the attribute for the user's name
-            Color: colors[colorIndex]
-        };
-    });
+  var data = [];
+  var colors = [
+    "#ff8787",
+    "#9775fa",
+    "#748ffc",
+    "#3bc9db",
+    "#69db7c",
+    "#fdd835",
+    "#748ffc",
+    "#9775fa",
+    "#df5286",
+    "#7fa900",
+    "#fec200",
+    "#5978ee",
+    "#00bdae",
+    "#ea80fc",
+  ];
+  // Ensure there are enough colors for the users.
+  // If not, add more colors to the array.
+  if (users.length > colors.length) {
+    throw new Error("Not enough colors for the number of users");
+  }
+
+  return users.map((user, index) => {
+    // Use the index to match the user with a color.
+    return {
+      Id: index + 1, // Assign a new ID based on the index to ensure uniqueness
+      Text: user.name, // Assuming 'name' is the attribute for the user's name
+      Color: colors[index], // Match color index with user index
+    };
+  });
 }
