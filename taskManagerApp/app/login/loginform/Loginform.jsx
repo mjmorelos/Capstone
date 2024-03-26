09ad726ca842d02ecba99086ef540a75d9540bae
mjmorelos <markjoseph.morelos@edu.sait.ca>
2024-03-25 20:10:06 -0600

    error msg on login

--- taskManagerApp/app/login/loginform/Loginform.jsx
+++ taskManagerApp/app/login/loginform/Loginform.jsx
@@ -1,19 +1,31 @@
 "use client";
+// Modify the login form to include error message handling and display
+import React, { useState } from 'react';
 import { authenticate } from "@/app/utility/action";
 import styles from "./loginform.module.css";
-import { useFormState } from "react-dom";
 
 const Loginform = () => {
-  const [state, formAction] = useFormState(authenticate, undefined);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    try {
+      const result = await authenticate(undefined, formData);
+      setErrorMessage(result); // Assuming `authenticate` returns error message on failure
+    } catch (error) {
+      setErrorMessage('The name or password provided is incorrect. Please try again.');
+    }
+  };
 
   return (
-    <form action={formAction} className={styles.form}>
+    <form onSubmit={handleSubmit} className={styles.form}>
       <h1 className={styles.title}>Sign In</h1>
       <p className={styles.welcome}>Welcome back!</p>
       <input type="text" placeholder="Enter name" name="name" />
       <input type="password" placeholder="Enter Password" name="password" />
-      <button>LOGIN</button>
-      {state && state}
+      {errorMessage && <div className={`${styles.error} ${styles.errorMessage}`}>{errorMessage}</div>}
+      <button type="submit">LOGIN</button>
       <span>
         <p className={styles.noaccount}>
           Don't have an account? <span>Sign Up</span>
